a12e58984cc76e74ff3ad00dda65c7698fd59590
Lubricat <lecorre.e@gmail.com>
2021-07-18 23:08:40 +0000

    Interaction serveurs

--- public/js/yugiLP.js
+++ public/js/yugiLP.js
@@ -20,33 +20,118 @@ if (!playerb.error) {
     playerb.start();
 }
 
+/** initiate toggle **/
+var player = 'A';
+
+$(function() {
+    $('#activeplayer').bootstrapToggle();
+
+    $('#activeplayer').change(function() {
+	player == 'A' ? player = 'B' : player = 'A';
+    })
+})
+
+
+/** keyboard listener **/
+
 $(document).on('keypress',function(e) {
+   var key = e.keyCode;
+	
+    document.getElementById("lpinteract").focus();
+	
+    if(e.which == 13) { //enter
+	
+		if ($('#lifea').text() == 0 || $('#lifeb').text() == 0) {
+			/** set default **/
+			ptsd.play();
+			playera.update(8000);
+			playerb.update(8000);
+			
+			setLP(ref, 'A', 8000);
+			setLP(ref, 'B', 8000);
+		} else {
+			/** calcul lp **/
+			var inter = $('#lpinteract').val();
+			var life = 0;
+			
+			if (player == 'A')
+				life = $('#lifea').text();
+			else
+				life = $('#lifeb').text();
 
-    if(e.which == 13) {
-	if ($('#lifea').text() == 0 || $('#lifeb').text() == 0) {
-	    ptsd.play();
-	    playera.update(8000);
-	    playerb.update(8000);	    
-	} else {
-	    var lifea = $('#lifea').text();
-	    var lifeb = $('#lifeb').text();
-	    var inter = $('#lpinteract').val();
+			if (inter == '')	    
+				inter = '-'+(parseInt(life)/2);
+			
+			life = parseInt(life) + parseInt(inter);
+			
+			if (life <= 0) {
+				ptsz.play();
+				life = 0;
+			} else {
+				ptsd.play();
+			}
+				
+			if (player == 'A')
+				playera.update(life);
+			else
+				playerb.update(life);
+			
+			/** envoie ajax **/
+			setLP(ref, player, life);
 
-	    if (inter == '') {
-		inter = '-'+(parseInt(lifea)/2);
-	    }
-	    
-	    var val = parseInt(lifea) + parseInt(inter);
-	    
-	    if (val == 0) {
-		ptsz.play();
-		playera.update(val);
-	    } else {
-		ptsd.play();
-		playera.update(val);
-	    }
-
-	    $('#lpinteract').val('');
-	}
+			/** reset champ saisie **/
+			$('#lpinteract').val('');
+		}
     }
+	
+    if(e.which == 32) {//space
+		e.preventDefault();
+		getLP(ref);
+	}
 });
+
+function setLP(ref, player, val) {
+	$.ajax({
+		type: "POST",
+		url: '/api/yugilp/'+ref+'/'+player+'/'+parseInt(val),
+	});
+}
+
+function updateLP(lpa, lpb) {
+	lifea = $('#lifea').text();
+	lifeb = $('#lifea').text();
+	
+	if (parseInt(lpa) != parseInt(lifea) || parseInt(lpb) != parseInt(lifeb)) {
+	
+		if (lpa == 0 || lpb == 0) {
+			ptsz.play();
+		} else {
+			ptsd.play();
+		}
+		
+		if (parseInt(lpa) != parseInt(lifea)) {
+			playera.update(lpa);
+		}
+		
+		if (parseInt(lpb) != parseInt(lifeb)) {
+			playerb.update(lpb);
+		}
+	}
+}
+    
+/** futur socket **/
+function getLP(ref) {
+    $.ajax({
+		type: 'POST',
+		url: '/api/yugilp/'+ref,
+		success: function(data) {
+			updateLP(data["1"], data["2"]);
+		}
+    });
+}
+
+/*$(document).ready(function() {
+    while(1) {
+		setTimeout(getLp(ref), 1000);
+    }
+});*/
--- src/Controller/YugiLPController.php
+++ src/Controller/YugiLPController.php
@@ -15,34 +15,79 @@ class YugiLPController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/YugiLPController.php',
-        ]);
+		return $this->redirectToRoute('yugi_lp_ref', ['ref' => "demo"]);
     }
 
     /**
      * @Route("/yugilp/{ref}/{playera}/{playerb}", name="yugi_lp_ref")
      */
-    public function show(string $ref, string $playera = 'A', string $playerb = 'B'): Response
+    public function show(string $ref, string $playera = 'Duelist A', string $playerb = 'Duelist B'): Response
     {
-        $lp = $this->getDoctrine()
-	         ->getRepository(YugiLP::class)
-		 ->findOneByRef($ref);
+		$lp = $this->getDoctrine()
+			->getRepository(YugiLP::class)
+			->findOneByRef($ref);
 
         if (!$lp) {
-	   $lp = new YugiLP();
-	   $lp->setRef($ref);
+			$lp = new YugiLP();
+			$lp->setRef($ref);
 
-	   $em = $this->getDoctrine()->getManager();
-	   $em->persist($lp);
-	   $em->flush();
-	}
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($lp);
+			$em->flush();
+		}
 
         return $this->render('lp.html.twig', [
-	       'lp' => $lp,
-	       "pa" => $playera,
-	       "pb" => $playerb
-	]);
+		    'lp' => $lp,
+		    "pa" => $playera,
+		    "pb" => $playerb
+		]);
+    }
+	
+	/**
+     * @Route("/api/yugilp/{ref}/{player}/{life}", methods={"POST"})
+     */
+    public function updateLP(string $ref, string $player, int $life): Response
+    {
+		$lp = $this->getDoctrine()
+			->getRepository(YugiLP::class)
+			->findOneByRef($ref);
+		
+		if ($lp) {
+			switch($player) {
+				case 'A':
+					$lp->setLpa($life);
+					break;
+				
+				case 'B':
+					$lp->setLpb($life);
+					break;
+			}
+			
+			$lp->setDateUpd(new \DateTime());
+			
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($lp);
+			$em->flush();
+			
+			return $this->json(['result' => 'OK']);
+		}
+		
+		return $this->json(['result' => 'KO', 'description' => 'Bad reference'], 404);
+    }
+	
+	/**
+     * @Route("/api/yugilp/{ref}", methods={"POST"})
+     */
+    public function getLP(string $ref): Response
+    {
+		$lp = $this->getDoctrine()
+			->getRepository(YugiLP::class)
+			->findOneByRef($ref);
+		
+		if ($lp) {
+			return $this->json(['result' => 'OK', 1 => $lp->getLpa(), 2 => $lp->getLpb()]);
+		}
+		
+		return $this->json(['result' => 'KO', 'description' => 'Bad reference'], 404);
     }
 }
